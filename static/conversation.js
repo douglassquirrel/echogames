746a969732ad00ba5e090bd77935e098800650cd
Douglas Squirrel <ds@douglassquirrel.com>
2020-12-27 19:29:31 +0000

    new game button

--- static/conversation.js
+++ static/conversation.js
@@ -1,8 +1,13 @@
-game_selector = get_element("games");
-session_id    = get_element("session_id");
-transcript    = get_element("transcript");
-line_input    = get_element("line_input");
-send_button   = get_element("send_button");
+game_selector   = get_element("games");
+session_id      = get_element("session_id");
+new_game_button = get_element("new_game_button");
+transcript      = get_element("transcript");
+line_input      = get_element("line_input");
+send_button     = get_element("send_button");
+
+function new_game() {
+    set_value(session_id, "echogames.session." + random(10000, 99999));
+}
 
 function print(line) { append(transcript, line + LINE_BREAK); }
 
@@ -15,6 +20,7 @@ function send_line() {
     send_request(get_value(session_id), get_value(game_selector), line, print);
 }
 
-set_value(session_id, "echogames.session." + random(10000, 99999));
+new_game();
+call_on_click(new_game_button, new_game);
 call_on_click(send_button, send_line);
 click_on_enter(line_input, send_button);

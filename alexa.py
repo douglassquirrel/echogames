cf9825e6f614c63f0d6c374992184c159c22fcd4
Douglas Squirrel <ds@douglassquirrel.com>
2020-12-30 13:43:44 +0000

    switch to class structure for alexa handlers

--- alexa.py
+++ alexa.py
@@ -1,3 +1,4 @@
+from ask_sdk_core.dispatch_components import AbstractExceptionHandler, AbstractRequestHandler
 from ask_sdk_core.handler_input import HandlerInput
 from ask_sdk_core.skill_builder import SkillBuilder
 from ask_sdk_core.utils import is_request_type, is_intent_name
@@ -15,34 +16,50 @@ def init_alexa(app):
 
 sb = SkillBuilder()
 
-@sb.request_handler(can_handle_func=is_request_type("LaunchRequest"))
-def launch_request_handler(handler_input):
-    # type: (HandlerInput) -> Response
-    line = "I'm agreeable, say something!"
+class LaunchRequestHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_request_type("LaunchRequest")(handler_input)
 
-    handler_input.response_builder.speak(line) \
-        .set_card(SimpleCard("Agreeable", line)) \
-        .set_should_end_session(False)
-    return handler_input.response_builder.response
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        line = "I'm agreeable, say something!"
 
-@sb.request_handler(can_handle_func=is_intent_name("AgreeIntent"))
-def agree_intent_handler(handler_input):
-    # type: (HandlerInput) -> Response
-    line = "I agree!"
+        handler_input.response_builder.speak(line) \
+            .set_card(SimpleCard("Agreeable", line)) \
+            .set_should_end_session(False)
+        return handler_input.response_builder.response
 
-    handler_input.response_builder.speak(line) \
-        .set_card(SimpleCard("Agreeable", line)) \
-        .set_should_end_session(True)
-    return handler_input.response_builder.response
+class AgreeIntentHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_intent_name("AgreeIntent")(handler_input)
 
-@sb.exception_handler(can_handle_func=lambda i, e: True)
-def all_exception_handler(handler_input, exception):
-    # type: (HandlerInput, Exception) -> Response
-    print(exception)
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        line = "I agree!"
 
-    line = "Sorry, I didn't understand, so I can't agree. Please try again!"
-    handler_input.response_builder.speak(line).ask(line)
-    return handler_input.response_builder.response
+        handler_input.response_builder.speak(line) \
+            .set_card(SimpleCard("Agreeable", line)) \
+            .set_should_end_session(True)
+        return handler_input.response_builder.response
+
+class AllExceptionHandler(AbstractExceptionHandler):
+    def can_handle(self, handler_input, exception):
+        # type: (HandlerInput, Exception) -> bool
+        return True
+
+    def handle(self, handler_input, exception):
+        # type: (HandlerInput, Exception) -> Response
+        print(exception)
+
+        line = "Sorry, I didn't understand, so I can't agree. Please try again!"
+        handler_input.response_builder.speak(line).ask(line)
+        return handler_input.response_builder.response
+
+sb.add_request_handler(LaunchRequestHandler())
+sb.add_request_handler(AgreeIntentHandler())
+sb.add_exception_handler(AllExceptionHandler())
 
 def create_alexa_handler(h, app):
     sa = SkillAdapter(skill=sb.create(), skill_id=ALEXA_SKILL_ID, app=app)
